f05aee393ea2e6ee034b5faccd5e444cf46a2292
juhye0p <leejuh1621@gmail.com>
2025-01-31 09:55:36 +0900

    commit

--- Injector.c
+++ Injector.c
@@ -20,7 +20,7 @@ DWORD GetPID() {
 
     if (Process32First(hProcess, &pe32)) {
         do {
-            if (!wcscmp(pe32.szExeFile, TEXT("Target.exe"))) {
+            if (!wcscmp(pe32.szExeFile, TEXT("[PROCESS NAME]"))) {
                 return pe32.th32ProcessID;
             }
         } while (Process32Next(hProcess, &pe32));
